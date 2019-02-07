b600e56301f46405fc8122b6a3476d02917781ee
Glerison <Glerisonlima@gmail.com>
2019-02-07 18:12:25 -0300

    update

---
+++ src/main/resources/static/js/promo-add.js
@@ -0,0 +1,15 @@
+//FUNÇÃO PARA CAPTURAR AS META TAGS
+$("#linkPromocao").on('change', function(){
+	var url = $(this).val();
+	
+	if(url.length > 7){
+		$.ajax({
+			method: "POST",
+			url: "/meta/info?url=" + url,
+			cache: false, 
+			success: function(data){
+				console.log(data);
+			}
+		});
+	}
+});
