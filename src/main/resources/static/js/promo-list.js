f00bad0b19eca484c9465b843a2d8d61b94af323
glerisonlima <glerisonlima@gmail.com>
2019-02-19 00:42:40 -0300

    aula 41

---
+++ src/main/resources/static/js/promo-list.js
@@ -0,0 +1,53 @@
+var pageNumber = 0;
+
+$(document).ready(function(){
+	$("#loader-img").hide();
+	$("#fim-btn").hide();
+})
+
+//efeito infinite scroll
+$(window).scroll(function(){
+	
+	var scrollTop = $(this).scrollTop();
+	var conteudo = $(document).height() - $(window).height();
+	
+	//console.log('crollTop: ', scrollTop, ' | ', 'conteudo', conteudo);
+	
+	if(scrollTop >= conteudo){
+		pageNumber++;
+		setTimeout(function(){
+			loadByScrollBar(pageNumber);
+		}, 200);
+	}
+	
+});
+
+function loadByScrollBar(pageNumber){
+	$.ajax({
+		method: "GET",
+		url: "/promocao/list/ajax",
+		data: {
+			page: pageNumber
+		},
+		beforeSend: function(){
+			$("#loader-img").show();
+		},
+		success: function(response){
+			//console.log("resposta > ", response);
+			if(response.length > 150){
+				$(".row").fadeIn(250, function(){
+					$(this).append(response);
+				});
+			}else{
+				$("#fim-btn").show();
+				$("#loader-img").removeClass();
+			}
+		},
+		error: function(){
+			alert("Ops, ocorreu um erro: "+ xhr.status + " - " + xhr.statusText);
+		},
+		complete: function(){
+			$("#loader-img").hide();
+		}
+	});
+}
